a0c3034bd7450183a1fcdf87b24df7a1de45bed4
Anton Alekseev <atix.wind@gmail.com>
2025-11-03 12:37:26 +0300

    Corrections and adjustments in M5:practic

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Snippet
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index_page(request):
@@ -14,12 +15,15 @@ def add_snippet_page(request):
 
 def snippet_view_page(request, id: int):
     
-    snippet = get_object_or_404(Snippet, pk=id)
-    context = {
-        'pagename': 'Просмотр сниппета',
-        'snippet': snippet,
-    }
-    return render(request, 'pages/view_snippet.html', context)
+    
+    context = {'pagename': 'Просмотр сниппета'}
+    try:
+        snippet = Snippet.objects.get(pk=id)
+    except ObjectDoesNotExist:
+        return render(request, 'pages/error.html', context | {"error": f'Snippet with id={id} not found.'})
+    else:
+        context['snippet'] = snippet
+        return render(request, 'pages/view_snippet.html', context)
 
 def snippets_page(request):
     snippets = Snippet.objects.all().order_by('id')
